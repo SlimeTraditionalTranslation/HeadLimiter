04f698e6524702bb231697f0550338dd7bd6062c
Jeffrey Kosse <j3fftw@gmail.com>
2021-01-11 23:46:39 +0100

    Multi threaded </3 walshy

--- src/main/java/dev/j3fftw/headlimiter/HeadLimiter.java
+++ src/main/java/dev/j3fftw/headlimiter/HeadLimiter.java
@@ -4,6 +4,8 @@ import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 import java.io.File;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
+import me.mrCookieSlime.Slimefun.cscorelib2.reflection.ReflectionUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -23,11 +25,6 @@ public final class HeadLimiter extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
-
     private boolean isCargo(SlimefunItem sfItem) {
         return sfItem.isItem(SlimefunItems.CARGO_INPUT_NODE)
             || sfItem.isItem(SlimefunItems.CARGO_OUTPUT_NODE)
@@ -39,22 +36,29 @@ public final class HeadLimiter extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlace(BlockPlaceEvent e) {
         final SlimefunItem sfItem = SlimefunItem.getByItem(e.getItemInHand());
-        if (e.getBlock().getType() == Material.PLAYER_HEAD
+        if ((e.getBlock().getType() == Material.PLAYER_HEAD || e.getBlock().getType() == Material.PLAYER_WALL_HEAD)
             && sfItem != null && isCargo(sfItem)
         ) {
-            int i = 0;
             final Block block = e.getBlock();
-            for (BlockState bs : block.getChunk().getTileEntities()) {
-                final SlimefunItem slimefunItem = BlockStorage.check(bs.getLocation());
-                if (slimefunItem != null && isCargo(slimefunItem))
-                    i++;
-            }
+            final BlockState[] te = block.getChunk().getTileEntities();
+            new Thread(() -> {
+                int i = 0;
+                for (BlockState bs : te) {
+                    final SlimefunItem slimefunItem = BlockStorage.check(bs.getLocation());
+                    if (slimefunItem != null && isCargo(slimefunItem))
+                        i++;
+                }
 
-            final int threshold = this.getConfig().getInt("amount");
-            if (i >= threshold) {
-                e.setCancelled(true);
-                e.getPlayer().sendMessage(ChatColor.RED + "You hit the limit of Cargo nodes in this chunk");
-            }
+                final int threshold = this.getConfig().getInt("amount");
+                if (i >= threshold) {
+                    Bukkit.getScheduler().runTask(this, () -> {
+                        block.setType(Material.AIR);
+                        block.getWorld().dropItemNaturally(block.getLocation(), sfItem.getItem());
+                    });
+                    BlockStorage.clearBlockInfo(block.getLocation());
+                    e.getPlayer().sendMessage(ChatColor.RED + "You hit the limit of Cargo nodes in this chunk");
+                }
+            }).start();
         }
     }
 }
