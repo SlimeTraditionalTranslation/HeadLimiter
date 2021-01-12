3fbbe7d6bdcc65daf6e08bbd321330d0077ca751
Jeffrey Kosse <j3fftw@gmail.com>
2021-01-12 22:31:34 +0100

    Added auto updates

--- src/main/java/dev/j3fftw/headlimiter/HeadLimiter.java
+++ src/main/java/dev/j3fftw/headlimiter/HeadLimiter.java
@@ -4,6 +4,7 @@ import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 import java.io.File;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
+import me.mrCookieSlime.Slimefun.cscorelib2.updater.GitHubBuildsUpdater;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -25,6 +26,10 @@ public final class HeadLimiter extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
 
         new Metrics(this, 9968);
+
+        if (getConfig().getBoolean("auto-update") && getDescription().getVersion().startsWith("DEV - ")) {
+            new GitHubBuildsUpdater(this, getFile(), "J3fftw1/HeadLimiter/master").start();
+        }
     }
 
     private boolean isCargo(SlimefunItem sfItem) {
