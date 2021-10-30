cbe52bc78ee2f31666839630cecb9b66c7ec4c38
Daniel Walsh <walshydev@gmail.com>
2021-10-30 11:35:15 +0100

    Refactor counts and checks.
    Also fixed an issue where the total was incorrect in the command

--- src/main/java/dev/j3fftw/headlimiter/HeadLimiter.java
+++ src/main/java/dev/j3fftw/headlimiter/HeadLimiter.java
@@ -1,6 +1,5 @@
 package dev.j3fftw.headlimiter;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.updater.GitHubBuildsUpdater;
@@ -11,33 +10,27 @@ import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.File;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
 
 public final class HeadLimiter extends JavaPlugin implements Listener {
 
     private static HeadLimiter instance;
-
-    private final ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("HeadLimiter-pool-%d").build();
-
-    public final ExecutorService executorService = Executors.newFixedThreadPool(
-        this.getConfig().getInt("thread-pool-size", 4), threadFactory
-    );
 
     @Override
     public void onEnable() {
         instance = this;
         if (!new File(getDataFolder(), "config.yml").exists())
             saveDefaultConfig();
+
+        Utils.loadPermissions();
 
         getServer().getPluginManager().registerEvents(this, this);
 
@@ -63,48 +56,18 @@ public final class HeadLimiter extends JavaPlugin implements Listener {
             || sfItem.isItem(SlimefunItems.CARGO_MANAGER);
     }
 
-    public void onCheck(int amount, Block block, BlockPlaceEvent e, int i, SlimefunItem sfItem) {
-        if (i > amount) {
-            Bukkit.getScheduler().runTask(this, () -> {
-                if (block.getType() != Material.AIR) {
-                    block.setType(Material.AIR);
-                    if (e.getPlayer().getGameMode() != GameMode.CREATIVE) {
-                        block.getWorld().dropItemNaturally(block.getLocation(), sfItem.getItem());
-                    }
-                }
-            });
-            BlockStorage.clearBlockInfo(block.getLocation());
-            e.getPlayer().sendMessage(ChatColor.RED + "You hit the limit of Cargo nodes in this chunk");
-        }
-    }
-
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlace(BlockPlaceEvent e) {
         final SlimefunItem sfItem = SlimefunItem.getByItem(e.getItemInHand());
+        final Player player = e.getPlayer();
+        final Block block = e.getBlock();
+
         if (!e.isCancelled()
-            && (e.getBlock().getType() == Material.PLAYER_HEAD || e.getBlock().getType() == Material.PLAYER_WALL_HEAD)
+            && (block.getType() == Material.PLAYER_HEAD || block.getType() == Material.PLAYER_WALL_HEAD)
             && sfItem != null && isCargo(sfItem)
         ) {
-            final Block block = e.getBlock();
-            final BlockState[] te = block.getChunk().getTileEntities();
-            executorService.submit(() -> {
-                int i = 0;
-                for (BlockState bs : te) {
-                    final SlimefunItem slimefunItem = BlockStorage.check(bs.getLocation());
-                    if (slimefunItem != null && isCargo(slimefunItem))
-                        i++;
-                }
-                if (this.getConfig().getBoolean("permissions", false)) {
-                    for (String perm : this.getConfig().getConfigurationSection("permission").getKeys(false)) {
-                        if (e.getPlayer().hasPermission("headlimiter.permission." + perm)) {
-                            onCheck(this.getConfig().getInt("permission." + perm), block, e, i, sfItem);
-                            break;
-                        }
-                    }
-                } else {
-                    onCheck(this.getConfig().getInt("amount"), block, e, i, sfItem);
-                }
-            });
+            final int maxAmount = Utils.getMaxHeads(player);
+            Utils.count(block.getChunk(), result -> Utils.onCheck(player, block, maxAmount, result.getTotal(), sfItem));
         }
     }
 
---
+++ src/main/java/dev/j3fftw/headlimiter/Utils.java
@@ -0,0 +1,99 @@
+package dev.j3fftw.headlimiter;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import me.mrCookieSlime.Slimefun.api.BlockStorage;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.function.Consumer;
+
+public final class Utils {
+
+    private static final ExecutorService SERVICE = Executors.newFixedThreadPool(
+        HeadLimiter.getInstance().getConfig().getInt("thread-pool-size", 4),
+        new ThreadFactoryBuilder().setNameFormat("HeadLimiter-pool-%d").build()
+    );
+
+    private static final Set<String> permissionNodes = new LinkedHashSet<>();
+    private static final String PERMISSION_PREFIX = "headlimiter.permission.";
+
+    private Utils() {}
+
+    protected static void loadPermissions() {
+        final ConfigurationSection sec = HeadLimiter.getInstance().getConfig().getConfigurationSection("permission");
+        if (sec != null) {
+            permissionNodes.addAll(sec.getKeys(false));
+        }
+    }
+
+    public static int getMaxHeads(@Nonnull Player player) {
+        if (permissionsEnabled() && !permissionNodes.isEmpty()) {
+            for (String node : permissionNodes) {
+                if (player.hasPermission(PERMISSION_PREFIX + node)) {
+                    return HeadLimiter.getInstance().getConfig().getInt("permission." + node);
+                }
+            }
+        }
+        return getDefaultMax();
+    }
+
+    public static boolean permissionsEnabled() {
+        return HeadLimiter.getInstance().getConfig().getBoolean("permissions", false);
+    }
+
+    public static int getDefaultMax() {
+        return HeadLimiter.getInstance().getConfig().getInt("amount", 25);
+    }
+
+    public static void count(@Nonnull Chunk chunk, @Nonnull Consumer<CountResult> consumer) {
+        final BlockState[] tileEntities = chunk.getTileEntities();
+
+        SERVICE.submit(() -> {
+            final Map<String, Integer> counts = new HashMap<>();
+            int total = 0;
+            for (BlockState state : tileEntities) {
+                final SlimefunItem slimefunItem = BlockStorage.check(state.getLocation());
+                if (slimefunItem != null) {
+                    counts.merge(slimefunItem.getId(), 1, Integer::sum);
+                    total++;
+                }
+            }
+
+            consumer.accept(new CountResult(total, counts));
+        });
+    }
+
+    @ParametersAreNonnullByDefault
+    public static void onCheck(Player player, Block block, int maxAmount, int count, SlimefunItem sfItem) {
+        if (count > maxAmount) {
+            Bukkit.getScheduler().runTask(HeadLimiter.getInstance(), () -> {
+                if (block.getType() != Material.AIR) {
+                    block.setType(Material.AIR);
+                    if (player.getGameMode() != GameMode.CREATIVE) {
+                        block.getWorld().dropItemNaturally(block.getLocation(), sfItem.getItem());
+                    }
+                }
+            });
+
+            BlockStorage.clearBlockInfo(block.getLocation());
+            player.sendMessage(ChatColor.RED + "You hit the limit of Cargo nodes in this chunk");
+        }
+    }
+}
